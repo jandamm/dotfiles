59f1cf1a61d10b9a1bcf0c075ff04f8478fa50e9
Jan Dammshäuser <mail@jandamm.de>
2020-03-21 22:33:36 +0100

    zsh: configure fzf

---
+++ zsh/after/fzf.zsh
@@ -0,0 +1,11 @@
+# Default options for fzf
+# - Use reverse layout
+# - Preview using bat or exa
+# - Quit when no selection is possible
+# - Select when only one selection is possible
+export FZF_DEFAULT_OPTS='--layout=reverse --preview "[ -f {} ] && bat --color=always {} || exa -G1 {}" -0 -1'
+
+# Use rg instead of find
+# rg is configured to ignore files/paths I don't want to search.
+export FZF_DEFAULT_COMMAND='rg --files --hidden'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
