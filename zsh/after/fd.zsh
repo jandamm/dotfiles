fdbd9d1c260ec1b2bdf6b5d10659b07c29eb85a2
Jan Dammshäuser <mail@jandamm.de>
2020-03-21 21:02:02 +0100

    zsh: restructure files

--- zsh/after/fd.zsh
+++ zsh/after/fd.zsh
@@ -1,16 +1,3 @@
-export RIPGREP_CONFIG_PATH=$HOME/.ripgreprc
-
-if hash rg 2>/dev/null; then
-	alias alse="alias | rg -i"
-	alias 'alias?'="alias | rg -i"
-	alias rip="rg -i"
-	alias '??'='rip'
-	function rgfd() {
-		rg --files --glob "**/*$1*/**" --glob "**$1**"
-	}
-else echo '[WARN] ripgrep is not installed'
-fi
-
 if hash fdfind 2>/dev/null
 then
 	alias fd=fdfind
