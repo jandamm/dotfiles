ba87da16129f182cc2349e7a7ee8758da4e8314d
Jan Dammshäuser <mail@jandamm.de>
2020-03-25 19:12:34 +0100

    zsh: adjust fzf default preview to not show errors

--- zsh/after/fzf.zsh
+++ zsh/after/fzf.zsh
@@ -3,7 +3,7 @@
 # - Preview using bat or exa
 # - Quit when no selection is possible
 # - Select when only one selection is possible
-export FZF_DEFAULT_OPTS='--layout=reverse --preview "[ -f {} ] && bat --color=always {} || exa -G1 {}" -0 -1'
+export FZF_DEFAULT_OPTS='--layout=reverse --preview "([ -f {} ] && bat --color=always {}) || ([ -d {} ] && exa -G1 {}) || echo {} " -0 -1'
 
 # Use rg instead of find
 # rg is configured to ignore files/paths I don't want to search.
