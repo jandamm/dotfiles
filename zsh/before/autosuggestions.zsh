17bad46ff8c0d1692f7c6bd8e330b13bd799a47f
Jan Dammshäuser <mail@jandamm.de>
2020-03-27 21:06:27 +0100

    zsh: improve autosuggestions
    Add prompt speed improvement and use completion as source for
    suggestions as well.

---
+++ zsh/before/autosuggestions.zsh
@@ -0,0 +1,6 @@
+# Use async suggestion fetching
+ZSH_AUTOSUGGEST_USE_ASYNC=1
+# Don't readd keybindings on every precmd
+ZSH_AUTOSUGGEST_MANUAL_REBIND=1
+# Autosuggest from history, then from completion
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
