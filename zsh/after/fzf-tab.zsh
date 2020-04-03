ecfedfb9c4e754de708982bc789bfd43b6fa5f2a
Jan Dammshäuser <mail@jandamm.de>
2020-04-03 22:02:37 +0200

    zsh: fix completion exa preview for fzf-tab

--- zsh/after/fzf-tab.zsh
+++ zsh/after/fzf-tab.zsh
@@ -14,8 +14,18 @@ zstyle ':fzf-tab:*' command $FZF_TAB_COMMAND
 # Disable fzf-preview as a default
 zstyle ':fzf-tab:*' extra-opts --preview=$extract''
 
-# give a preview of directory by exa when completing cd
+# extract values in zstyle
+# (experimental, may change in the future)
+local extract="
+# trim input(what you select)
+in=\${\${\"\$(<{f})\"%\$'\0'*}#*\$'\0'}
+# get ctxt for current completion(some thing before or after the current word)
+local -A ctxt=(\"\${(@ps:\2:)CTXT}\")
+"
+
+# give a preview of directory by exa when completing cd/z
 zstyle ':fzf-tab:complete:cd:*' extra-opts --preview=$extract'exa -1 --color=always ${~ctxt[hpre]}$in'
+zstyle ':fzf-tab:complete:z:*' extra-opts --preview=$extract'exa -1 --color=always ${~ctxt[hpre]}$in'
 
 # group commands and show description
 zstyle ':completion:*' format '---- %d'
