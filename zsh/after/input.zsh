f4999edd3d5f22898956b181e825c031146e7976
Jan Dammshäuser <mail@jandamm.de>
2020-04-09 18:21:38 +0200

    zsh: remove unnecessary check for cursor working
    This also interefered with "woefe/vi-mode.zsh" as both defined
    zle-line-init without wrapping previous definitions.

--- zsh/after/input.zsh
+++ zsh/after/input.zsh
@@ -1,16 +1,3 @@
-# Make sure that the terminal is in application mode when zle is active, since
-# only then values from $terminfo are valid
-if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-  function zle-line-init() {
-    echoti smkx
-  }
-  function zle-line-finish() {
-    echoti rmkx
-  }
-  zle -N zle-line-init
-  zle -N zle-line-finish
-fi
-
 # Expand !gs to last command starting with gs
 # ^gs to remove first 'gs' from last command
 bindkey ' ' magic-space
