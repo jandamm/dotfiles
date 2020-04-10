d71cca89aca265fedd9b9e490fa2ddbe948c9943
Jan Dammshäuser <mail@jandamm.de>
2020-04-10 20:50:14 +0200

    zsh: add back setting terminal to application mode
    I've disabled this as it did conflict with my vi-mode plugin. As this
    conflict is resolved I can add this back.

---
+++ zsh/before/input.zsh
@@ -0,0 +1,14 @@
+# Make sure that the terminal is in application mode when zle is active, since
+# only then values from $terminfo are valid
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+	function zle-line-init() {
+		echoti smkx
+	}
+
+	function zle-line-finish() {
+		echoti rmkx
+	}
+
+	zle -N zle-line-init
+	zle -N zle-line-finish
+fi
