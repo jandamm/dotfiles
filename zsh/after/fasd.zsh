f8bb496e4d364d6305135b8f2fe754e56cddb1fb
Jan Dammshäuser <mail@jandamm.de>
2020-05-23 21:26:16 +0200

    zsh: fix zz command
    zz broke by introducing setopt extendedglob. The new version does work
    with and without extendedglob enabled.

--- zsh/after/fasd.zsh
+++ zsh/after/fasd.zsh
@@ -11,6 +11,6 @@ alias z='fasd_cd -d'
 function zz() {
 	cd "$(fasd -lRd $1 \
 		| sed "s_^${HOME}_~_" \
-		| fzf -1 -0 --preview "exa -lhg --git --color=always \$(echo {} | sed "s_^~_${HOME}_")" \
+		| fzf -1 -0 --preview 'exa -lhg --git --color=always $(echo {} | sed "s_^~_${HOME}_")' \
 		| sed "s_^~_${HOME}_")"
 }
