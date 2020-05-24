9b6b37437204bb3f1e3edfc0ffe2cfe14e090fb3
Jan Dammshäuser <mail@jandamm.de>
2020-05-24 21:05:27 +0200

    zsh: enable extendedglob for zcompdump

--- zsh/login.zsh
+++ zsh/login.zsh
@@ -1,5 +1,8 @@
 # Execute code in the background
 {
+	# Activate extendedglob inside this function. (May be deactivated in global at some point)
+	# This would break this code - probably without noticing.
+	setopt extendedglob local_options
 	# Recreate zcompdump if it is older than 20 hours.
 	_comp_files=($HOME/.zsh/zcompdump_$ZSH_VERSION.zsh(Nm-20))
 	if ! (( $#_comp_files )); then
