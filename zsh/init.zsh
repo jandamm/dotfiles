849e0331958cea261f201c5ce52982f4c1e3a097
Jan Dammshäuser <mail@jandamm.de>
2020-06-02 22:52:36 +0200

    vim: update vim-abolisher

--- zsh/init.zsh
+++ zsh/init.zsh
@@ -97,9 +97,6 @@ zinit light clvv/fasd
 zinit ice as'program' pick'bin/git-dsf'
 zinit light zdharma/zsh-diff-so-fancy
 
-zinit ice as'program' pick'bin/abolisher'
-zinit light jandamm/abolisher
-
 # }}}
 
 # Completion {{{
