474ebc2a98ca01b7387b698cf6d5b842f6ec0d33
Jan Dammshäuser <mail@jandamm.de>
2021-11-22 19:33:36 +0100

    vim: lua add unload function

--- nvim/.config/nvim/lua/my/lua.lua
+++ nvim/.config/nvim/lua/my/lua.lua
@@ -1,9 +1,20 @@
+--- Unload a module
+---@param module string
+function _G.unload(module)
+	require('plenary.reload').reload_module(module, true)
+end
+
 --- Unload and require a module
+---@param module string
+---@return string
 function _G.reload(module)
-	require'plenary.reload'.reload_module(module, true)
+	unload(module)
 	return require(module)
 end
 
+--- Load an module if it is available
+---@param module string
+---@return string
 function _G.prequire(module)
 	local ok, mod = pcall(require, module)
 	return ok and mod
