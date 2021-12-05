06f4eaa12a6ca1401d1c0a96fd60be0bb69a5c0a
Jan Dammshäuser <mail@jandamm.de>
2021-12-05 20:53:38 +0100

    vim: disable diagnostics in insert mode
    Ideally I'd also disable it in select mode but I haven't found a
    matching autocmd while glimpsing at the help.

--- nvim/.config/nvim/plugin/diagnostics.lua
+++ nvim/.config/nvim/plugin/diagnostics.lua
@@ -1,4 +1,4 @@
-local s = require('my.symbols')
+local s = require 'my.symbols'
 local groups = {
 	{ name = 'Error', symbol = s.E },
 	{ name = 'Warn', symbol = s.W },
@@ -10,3 +10,13 @@ for _, sign in pairs(groups) do
 	local name = 'DiagnosticSign' .. sign.name
 	vim.fn.sign_define(name, { text = sign.symbol, texthl = name, linehl = '', numhl = '' })
 end
+
+-- Disable diagnostics in insert mode
+-- TODO: Use ModeChanged when released
+vim.cmd [[
+	augroup diagnostics
+		autocmd!
+		autocmd InsertEnter * lua vim.diagnostic.hide()
+		autocmd InsertLeave * lua vim.diagnostic.show()
+	augroup END
+]]
