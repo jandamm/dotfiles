d6c10367e67c5bbaeda4d34888848568c681b941
Jan Dammshäuser <mail@jandamm.de>
2021-12-14 22:25:58 +0100

    vim: lua add put function

--- nvim/.config/nvim/lua/my/lua.lua
+++ nvim/.config/nvim/lua/my/lua.lua
@@ -1,13 +1,19 @@
---- Unload a module
+---Unload a module
 ---@param module string
 function _G.unload(module)
 	require('plenary.reload').reload_module(module, true)
 end
 
---- Unload and require a module
+---Unload and require a module
 ---@param module string
 ---@return string
 function _G.reload(module)
 	unload(module)
 	return require(module)
+end
+
+---Inspect and log the object
+---@param object any
+function _G.put(object)
+	print(vim.inspect(object))
 end
