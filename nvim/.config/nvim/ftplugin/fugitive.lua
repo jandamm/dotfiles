a8f9055c3bb063630600db968b947c963da9afa2
Jan Dammshäuser <mail@jandamm.de>
2022-10-20 12:41:57 +0200

    vim: update shortcuts for git-pr --stash

--- nvim/.config/nvim/ftplugin/fugitive.lua
+++ nvim/.config/nvim/ftplugin/fugitive.lua
@@ -1,8 +1,8 @@
 require('my.map').buffer.register({
 	po = { ':GG pr open ', 'New PR for current Branch', noremap = true },
-	pp = { '.GG pr new ', 'New PR' },
+	pp = { '.GG pr new --stash ', 'New PR' },
 	ps = { '.GG pr switch ', 'Switch to PR branch' },
-	pu = { '.GG pr update ', 'Update PR' },
+	pu = { '.GG pr update --stash ', 'Update PR' },
 }, 'n', { noremap = false, silent = false })
 
 vim.cmd [[nmap <buffer> p <Cmd>lua require("which-key").show("p", {mode = "n", auto = true})<CR>]]
