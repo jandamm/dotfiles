c730398f734f29cdcaba66d7cecd61f0f5d04a54
Jan Dammshäuser <mail@jandamm.de>
2022-12-14 21:28:37 +0100

    vim: fix gitsigns missing mappings
    In a recent update the default mappings have been removed and replaced
    by an example config. I have copied the needed config from the readme.

--- nvim/.config/nvim/lua/my/packer.lua
+++ nvim/.config/nvim/lua/my/packer.lua
@@ -176,6 +176,39 @@ use {
 					add = { text = '┃' },
 					change = { text = '┃' },
 				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					-- Navigation
+					map('n', ']c', function()
+						if vim.wo.diff then
+							return ']c'
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return '<Ignore>'
+					end, { expr = true })
+
+					map('n', '[c', function()
+						if vim.wo.diff then
+							return '[c'
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return '<Ignore>'
+					end, { expr = true })
+
+					-- Text object
+					map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+				end,
 			}
 			vim.cmd [[
 				omap ah ih
@@ -183,7 +216,7 @@ use {
 			]]
 		end,
 		requires = 'nvim-lua/plenary.nvim',
-		tag = 'release',
+		-- tag = 'release',
 		event = 'VimEnter', -- Otherwise it produces an error when a :Git buffer is open.
 	},
 	{
